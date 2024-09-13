0141dc4631ab24f42c1c5db32133099fcf30576c
Patchaiyappan K <patchaiyappank@prometheansoftwares.com>
2024-09-13 08:17:08 +0530

    re

--- Organization_Admin_Client/src/loginCredentials.js
+++
@@ -1,9 +0,0 @@
-// loginCredentials.js
-export const getLoginCredentials = async () => {
-    const response = await fetch( `${baseApiUrl}/loginusername`); 
-    const data = await response.json();
-    return { 
-      organization_name: data.organization_name 
-    };
-  };
-  
